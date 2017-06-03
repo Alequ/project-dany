5007653776192669b40ebeffa218f53bac384ac3
Alex Turcu <turcu.alex5@gmail.com>
2017-06-03 11:42:46 +0300

    v2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 
+/*Filter  */
+$(function() {
+
+    var ft = $.filtrify("productList", "here", {
+        close : true,
+        callback : function( query, match, mismatch ) {
+            if ( mismatch.length ) $("div#reset").show();
+            else $("div#reset").hide();
+        }
+    });
+
+    $("div#reset span").click(function() {
+        ft.reset();
+    });
+
+});
+
+/*Jpages  */
+
+$(function() {
+    /* initiate plugin */
+    $("div.holder").jPages({
+        containerID: "productList",
+        perPage : 16
+    });
+});
