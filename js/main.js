2c2d149855f7227902ff3963f75c091614640e5b
Alex Turcu <turcu.alex5@gmail.com>
2017-09-27 20:14:38 +0300

    ss

--- js/main.js
+++ js/main.js
@@ -4,13 +4,14 @@ const latime = document.querySelector(".latime");
 const raza = document.querySelector(".raza");
 const sezon = document.querySelector(".sezon");
 const output = document.querySelector("#productList");
+const reset = document.querySelector(".reset");
 
 produse.forEach(produs => {
   output.innerHTML += `
       <li class="products" >
         <div class="col-md-3 ">
           <div class="product">
-             <img src="${produs.image}" alt=""/>
+             <img class="zoom" src="${produs.image}" alt=""/>
              <h3>${produs.titlu}</h3>
              <div class="price">
             <p>${produs.pret}</p>
@@ -24,7 +25,7 @@ produse.forEach(produs => {
 
 
 function getValues(e) {
-    e.preventDefault();
+  e.preventDefault();
 
   const result =  produse.filter(function (el) {
       return el.inaltime == inaltime.value &&
@@ -40,7 +41,7 @@ function getValues(e) {
       <li >
         <div class="col-md-3">
           <div class="product">
-             <img src="${res.image}" alt=""/>
+             <img class="zoom" src="${res.image}" alt=""/>
              <h3>${res.titlu}</h3>
              <div class="price">
             <p>${res.pret}</p>
@@ -50,16 +51,25 @@ function getValues(e) {
       </li>
 
       `
-
     })
-  } else {
-    output.innerHTML = "";
-    output.innerHTML = `
-      <p>Ne pare rau, nici un produs nu a fost gasit</p>
-    `
-  }
+    } else {
+      output.innerHTML = "";
+      reset.style.display = "initial";
+      output.innerHTML = `
+      <div class="alert alert-danger" role="alert" style="margin-top:30px;">
+      <strong>Nu am gasit niciun model corespunzator! <br>
+      </div>
+
+
+
+      `
+    }
 
 }
 
-
+$(".zoom").elevateZoom({
+  zoomType				: "lens",
+  lensShape : "round",
+  lensSize    : 200
+});
 submit.addEventListener("click", getValues)
